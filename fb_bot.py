131296d48d83198cbdd1908d254235866c1b6697
pavas <70338150+pavas780@users.noreply.github.com>
2021-05-05 14:40:38 +0530

    Create fb_bot.py

---
+++ fb_bot.py
@@ -0,0 +1,10 @@
+
+from selenium import webdriver
+browser=webdriver.Chrome('C:\\chrome\\chromedriver.exe')
+browser.get('https://www.facebook.com/')
+e=browser.find_element_by_id("email")
+e.send_keys("enter your phone number or email")
+d=browser.find_element_by_id("pass")
+d.send_keys("enter your password btw this")
+log=browser.find_element_by_id("u_0_b")
+log.click()
